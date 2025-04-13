273601d935c0eba53ca1ebfc085b28460805bdc0
Davis Muro <git@davisraym.com>
2025-04-12 18:52:09 -0700

    feat(storage): add row structure

---
+++ src/storage/mod.rs
@@ -0,0 +1,50 @@
+pub mod row;
+
+pub mod header {
+    pub mod row {
+        use crate::statement::{EMAIL_MAX_LENGTH, USERNAME_MAX_LENGTH};
+
+        pub(crate) const ROW_ID_SIZE: usize = size_of::<usize>();
+        pub(crate) const ROW_USERNAME_SIZE: usize = size_of::<char>() * USERNAME_MAX_LENGTH;
+        pub(crate) const ROW_EMAIL_SIZE: usize = size_of::<char>() * EMAIL_MAX_LENGTH;
+        pub(crate) const ROW_BODY_SIZE: usize = ROW_ID_SIZE + ROW_USERNAME_SIZE + ROW_EMAIL_SIZE;
+        pub(crate) const LEAF_ROW_SIZE: usize = ROW_BODY_SIZE;
+        pub(crate) const INTERNAL_ROW_SIZE: usize = ROW_ID_SIZE;
+
+        pub(crate) const ROW_ID: usize = 0;
+        pub(crate) const ROW_USERNAME: usize = ROW_ID + ROW_ID_SIZE;
+        pub(crate) const ROW_EMAIL: usize = ROW_USERNAME + ROW_USERNAME_SIZE;
+    }
+
+    pub mod page {
+        use super::row::{INTERNAL_ROW_SIZE, LEAF_ROW_SIZE};
+
+        pub(crate) const PAGE_SIZE: usize = 4096;
+        pub(crate) const PAGE_ID_SIZE: usize = size_of::<usize>();
+        pub(crate) const PAGE_CELLS_SIZE: usize = size_of::<usize>();
+        pub(crate) const PAGE_KIND_SIZE: usize = size_of::<u8>();
+        pub(crate) const PAGE_HEADER_SIZE: usize = PAGE_ID_SIZE + PAGE_CELLS_SIZE + PAGE_KIND_SIZE;
+
+        pub(crate) const PAGE_ID: usize = 0;
+        pub(crate) const PAGE_CELLS: usize = PAGE_ID + PAGE_ID_SIZE;
+        pub(crate) const PAGE_KIND: usize = PAGE_CELLS + PAGE_CELLS_SIZE;
+        pub(crate) const HEADER_END: usize = PAGE_HEADER_SIZE;
+
+        pub(crate) const CELLS_PER_LEAF: usize = (PAGE_SIZE - PAGE_HEADER_SIZE) / LEAF_ROW_SIZE;
+        pub(crate) const CELLS_PER_INTERNAL: usize =
+            (PAGE_SIZE - PAGE_HEADER_SIZE) / INTERNAL_ROW_SIZE;
+    }
+}
+
+pub mod error {
+    use thiserror::Error;
+
+    #[derive(Debug, Error)]
+    pub enum StorageError {
+        #[error("[row error][{action}]: {error}")]
+        Row { action: String, error: String },
+
+        #[error("[utility][{name}] error. reason: {cause:?}")]
+        Utility { name: String, cause: Option<String> },
+    }
+}
---
+++ src/storage/row.rs
@@ -0,0 +1,176 @@
+use super::{error::StorageError, header::row::*};
+
+pub(crate) const ROW_ALLOCATED_SPACE: usize = if INTERNAL_ROW_SIZE > LEAF_ROW_SIZE {
+    INTERNAL_ROW_SIZE
+} else {
+    LEAF_ROW_SIZE
+};
+
+pub(crate) struct Row(pub [u8; ROW_ALLOCATED_SPACE]);
+
+pub fn byte_to_char(bytes: &[u8], mut cause: Option<String>) -> Result<Vec<char>, StorageError> {
+    let mut out = Vec::new();
+
+    for chunk in bytes.chunks(4) {
+        let ch = std::str::from_utf8(&chunk)
+            .map_err(|_| StorageError::Utility {
+                name: "byte_to_char - read character".into(),
+                cause: cause.take(),
+            })?
+            .chars()
+            .next()
+            .ok_or(StorageError::Utility {
+                name: "byte_to_char - retrieve character".into(),
+                cause: cause.take(),
+            })?;
+        out.push(ch);
+    }
+
+    Ok(out)
+}
+
+pub fn char_to_byte(chars: &[char]) -> Vec<u8> {
+    let mut out = Vec::new();
+
+    for ch in chars {
+        let mut buf = [0; 4];
+        ch.encode_utf8(&mut buf);
+        out.extend_from_slice(&buf[..]);
+    }
+
+    out
+}
+
+impl Row {
+    pub fn new() -> Self {
+        Self([0; ROW_ALLOCATED_SPACE])
+    }
+
+    pub fn id(&self) -> Result<usize, StorageError> {
+        Ok(usize::from_ne_bytes(
+            self.0[ROW_ID..ROW_USERNAME]
+                .try_into()
+                .map_err(|_| StorageError::Row {
+                    action: "retrieve id".into(),
+                    error: "failed to get id bytes".into(),
+                })?,
+        ))
+    }
+
+    pub fn set_id(&mut self, id: usize) {
+        self.0[ROW_ID..ROW_USERNAME].clone_from_slice(id.to_ne_bytes().as_ref());
+    }
+
+    pub fn username(&self) -> Result<String, StorageError> {
+        let bytes = &self.0[ROW_USERNAME..ROW_EMAIL];
+        let chars = byte_to_char(bytes, Some(format!("row({}) username", self.id()?)))?;
+        Ok(chars.iter().collect())
+    }
+
+    pub fn set_username(&mut self, username: &[char]) {
+        self.0[ROW_USERNAME..ROW_EMAIL].clone_from_slice(&char_to_byte(username));
+    }
+
+    pub fn email(&self) -> Result<String, StorageError> {
+        let bytes = &self.0[ROW_EMAIL..self.0.len()];
+        let chars = byte_to_char(bytes, Some(format!("row({}) email", self.id()?)))?;
+        Ok(chars.iter().collect())
+    }
+
+    pub fn set_email(&mut self, email: &[char]) {
+        let end = self.0.len();
+        self.0[ROW_EMAIL..end].clone_from_slice(&char_to_byte(email));
+    }
+}
+
+impl TryFrom<&[u8]> for Row {
+    type Error = StorageError;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        let mut buf = [0; ROW_ALLOCATED_SPACE];
+        match value.len() {
+            size if size == LEAF_ROW_SIZE || size == INTERNAL_ROW_SIZE => {
+                buf[..size].clone_from_slice(&value[..]);
+                Ok(Self(buf))
+            }
+            l => Err(StorageError::Row {
+                action: "read bytes".into(),
+                error: format!("unexpected size '{}'", l),
+            }),
+        }
+    }
+}
+
+impl Into<[u8; INTERNAL_ROW_SIZE]> for Row {
+    fn into(self) -> [u8; INTERNAL_ROW_SIZE] {
+        let mut buf = [0; INTERNAL_ROW_SIZE];
+        buf[..].clone_from_slice(&self.0[..INTERNAL_ROW_SIZE]);
+        buf
+    }
+}
+
+impl Into<[u8; LEAF_ROW_SIZE]> for Row {
+    fn into(self) -> [u8; LEAF_ROW_SIZE] {
+        let mut buf = [0; LEAF_ROW_SIZE];
+        buf[..].clone_from_slice(&self.0[..LEAF_ROW_SIZE]);
+        buf
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::statement::{EMAIL_MAX_LENGTH, USERNAME_MAX_LENGTH, convert_to_char_array};
+
+    use super::*;
+
+    #[test]
+    fn char_to_byte_convertable() {
+        let chars = vec!['a', 'b'];
+        let bytes = char_to_byte(&chars);
+        assert_eq!(chars, byte_to_char(&bytes, None).unwrap())
+    }
+
+    #[test]
+    fn internal_cell() {
+        let mut row = Row::new();
+        row.set_id(90);
+
+        let bytes: [u8; INTERNAL_ROW_SIZE] = row.into();
+        let row: Row = (&bytes[..]).try_into().unwrap();
+        assert_eq!(row.id().unwrap(), 90);
+    }
+
+    #[test]
+    fn leaf_cell() {
+        let mut row = Row::new();
+        let email = convert_to_char_array::<EMAIL_MAX_LENGTH>(vec!['a', 'b'], '\0').unwrap();
+        row.set_email(&email);
+
+        let bytes: [u8; LEAF_ROW_SIZE] = row.into();
+        let row: Row = (&bytes[..]).try_into().unwrap();
+        assert_eq!(row.email().unwrap(), email.iter().collect::<String>());
+    }
+
+    #[test]
+    fn row_id() {
+        let mut row = Row::new();
+        row.set_id(10);
+        assert_eq!(row.id().unwrap(), 10);
+    }
+
+    #[test]
+    fn row_username() {
+        let mut row = Row::new();
+        let username = convert_to_char_array::<USERNAME_MAX_LENGTH>(vec!['a', 'b'], '\0').unwrap();
+        row.set_username(&username);
+        assert_eq!(row.username().unwrap(), username.iter().collect::<String>());
+    }
+
+    #[test]
+    fn row_email() {
+        let mut row = Row::new();
+        let email = convert_to_char_array::<EMAIL_MAX_LENGTH>(vec!['a', 'b'], '\0').unwrap();
+        row.set_email(&email);
+        assert_eq!(row.email().unwrap(), email.iter().collect::<String>());
+    }
+}
