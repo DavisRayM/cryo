5925e0bddf764e9d6cd976e2b81cc2b82a36afd5
Davis Muro <git@davisraym.com>
2025-04-20 15:54:33 -0700

    refactor: use `clap` crate for CLI

--- src/bin/cryo_cli.rs
+++ src/bin/cryo_cli.rs
@@ -1,16 +1,36 @@
+use clap::Parser;
 use std::{
     error::Error,
     io::{self, BufRead, Write},
+    path::PathBuf,
 };
 
-use cryo::Command;
+use cryo::{
+    Command,
+    storage::{StorageEngine, btree::BTree, pager::Pager},
+};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Path to storage directory
+    path: PathBuf,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Initialize env_logger; For logging to STDOUT/STDERR
     env_logger::init();
 
+    let cli = Cli::parse();
     let mut stdio = io::stdin().lock();
     let mut stdout = io::stdout().lock();
+
+    if !cli.path.is_dir() {
+        panic!("'{:?}' is not a directory", cli.path);
+    }
+    let store = cli.path.join("cryo.db");
+    let pager = Pager::open(store)?;
+    let mut btree = BTree::new(pager)?;
 
     loop {
         let mut s = String::default();
@@ -22,9 +42,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         match <&str as TryInto<Command>>::try_into(s.as_str()) {
             Ok(cmd) => match cmd {
-                Command::Exit => break,
+                c if c == Command::Exit => {
+                    btree.execute(c)?;
+                    break;
+                }
                 c => {
-                    println!("command: {c:?}")
+                    btree.execute(c)?;
                 }
             },
             Err(e) => eprintln!("error: {e}"),
